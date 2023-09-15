7b331dfc8164da04fa04798fe9d8a41b94b5d0d2
Carlos Oliva <carlos.oliva@eviden.com>
2023-09-15 14:12:14 +0200

    Subidos recursos app

--- front/chat-app/src/app/app-routing.module.ts
+++ front/chat-app/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ChatComponent } from './chat/chat.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat/chat.component'
 
 const routes: Routes = [
-  {path: '', redirectTo: 'chat', pathMatch: 'full'},
-  {path: 'chat', component: ChatComponent},
+  { path: '', redirectTo: 'chat', pathMatch: 'full' },
+  { path: 'chat', component: ChatComponent }
 ];
 
 @NgModule({
